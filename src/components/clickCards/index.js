7471b07b91a919cb55d7e80b7751eea205b159f4
KimblesAndBits <wearethekimbles@gmail.com>
2019-08-09 15:03:21 -0700

    some small file additions

---
+++ src/components/clickCards/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import characters from "../../characters.json";
+
+class ClickCard extends Component {
+    state = {
+        characters
+    }
+}
+
+export default ClickCard;
