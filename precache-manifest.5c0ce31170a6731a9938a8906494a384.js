2515bbc8c665716d5822ad8f89366b0a57680ba9
KimblesAndBits <wearethekimbles@gmail.com>
2019-08-13 17:58:09 -0700

    Updates

---
+++ precache-manifest.5c0ce31170a6731a9938a8906494a384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a469e5465d7ffe767882e79fe87182",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ed26f79ce705a48f4a7d",
+    "url": "/clicky-game/static/css/main.f844a74c.chunk.css"
+  },
+  {
+    "revision": "61477f81220081e1df57",
+    "url": "/clicky-game/static/js/2.901827b4.chunk.js"
+  },
+  {
+    "revision": "ed26f79ce705a48f4a7d",
+    "url": "/clicky-game/static/js/main.372cb00a.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "09073a37f0be2eab7f208442641a2272",
+    "url": "/clicky-game/static/media/crewniverse_font-webfont.09073a37.woff2"
+  },
+  {
+    "revision": "f093e442fc9938f107c17e8aed182898",
+    "url": "/clicky-game/static/media/crewniverse_font-webfont.f093e442.woff"
+  }
+]);
